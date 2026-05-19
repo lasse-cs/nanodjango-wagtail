a1126b26acdecff00881a7a40dc27dfac77e0861
lasse-cs <lasse.schmieding@gmail.com>
2026-05-19 20:50:06 +0100

    Clarify what is happening

--- nanotail.py
+++ nanotail.py
@@ -8,10 +8,6 @@ from nanodjango.urls import urlpatterns
 
 @hookimpl
 def django_pre_setup(app: Django):
-    _prepare = app._prepare
-
-    from django.conf import settings
-
     apps = [
         "wagtail.contrib.forms",
         "wagtail.contrib.redirects",
@@ -29,33 +25,44 @@ def django_pre_setup(app: Django):
     ]
 
     for wagtail_app in apps:
-        if wagtail_app in settings.INSTALLED_APPS:
+        if wagtail_app in app.settings.INSTALLED_APPS:
             continue
-        settings.INSTALLED_APPS.append(wagtail_app)
+        app.settings.INSTALLED_APPS.append(wagtail_app)
 
     redirect_middleware = "wagtail.contrib.redirects.middleware.RedirectMiddleware"
-    settings.MIDDLEWARE.append(redirect_middleware)
+    app.settings.MIDDLEWARE.append(redirect_middleware)
 
+    _prepare = app._prepare
+
+    # We need to monkeypatch _prepare in order
+    # to ensure that the wagtail catchall url is added last
     def monkey_prepare(self, is_prod=False):
         if self._prepared:
             return
 
+        # If have admin but no admin url
+        # move the django-admin to "django-admin/"
+        if not self.settings.ADMIN_URL and self.has_admin:
+            self.settings.ADMIN_URL = "django-admin/" 
+
         _prepare(is_prod)
 
-        from wagtail import urls as wagtail_urls
+        # Add the wagtail admin urrls
         from wagtail.admin import urls as wagtailadmin_urls
-        from wagtail.documents import urls as wagtaildocs_urls
-        
         urlpatterns.append(
             path("admin/", include(wagtailadmin_urls)),
         )
-        urlpatterns.append(
-            path("documents/", include(wagtaildocs_urls)),
-        )
+
+        # Need to add wagtail catch all url as last
+        from wagtail import urls as wagtail_urls
         urlpatterns.append(re_path(r"", include(wagtail_urls)))
 
     app._prepare = MethodType(monkey_prepare, app)
 
 @hookimpl
 def django_post_setup(app: Django):
-    pass
+    # Add the wagtail document urls
+    from wagtail.documents import urls as wagtaildocs_urls
+    urlpatterns.append(
+        path("documents/", include(wagtaildocs_urls)),
+    )
