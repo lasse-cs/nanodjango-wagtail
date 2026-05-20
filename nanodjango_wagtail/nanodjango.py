d3b2dc5e74f0e749972a53e585ab351be0a4fc42
lasse-cs <lasse.schmieding@gmail.com>
2026-05-20 07:12:17 +0100

    Format with ruff

--- nanodjango_wagtail/nanodjango.py
+++ nanodjango_wagtail/nanodjango.py
@@ -43,26 +43,30 @@ def django_pre_setup(app: Django):
         # If have admin but no admin url
         # move the django-admin to "django-admin/"
         if not self.settings.ADMIN_URL and self.has_admin:
-            self.settings.ADMIN_URL = "django-admin/" 
+            self.settings.ADMIN_URL = "django-admin/"
 
         _prepare(is_prod)
 
         # Add the wagtail admin urrls
         from wagtail.admin import urls as wagtailadmin_urls
+
         urlpatterns.append(
             path("admin/", include(wagtailadmin_urls)),
         )
 
         # Need to add wagtail catch all url as last
         from wagtail import urls as wagtail_urls
+
         urlpatterns.append(re_path(r"", include(wagtail_urls)))
 
     app._prepare = MethodType(monkey_prepare, app)
+
 
 @hookimpl
 def django_post_setup(app: Django):
     # Add the wagtail document urls
     from wagtail.documents import urls as wagtaildocs_urls
+
     urlpatterns.append(
         path("documents/", include(wagtaildocs_urls)),
     )
